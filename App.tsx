8dd032d8a4a1b86e623cc47e3b136d80515ca3f3
akshat-khosya <akshatdps12@gmail.com>
2022-08-16 21:10:34 +0530

    component && file structure

--- App.tsx
+++ App.tsx
@@ -1,12 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import { Provider } from 'react-native-paper';
+import Button from './src/components/Button';
+import TextInput from './src/components/TextInput';
+import { theme } from './src/core/theme';
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider theme={theme}>
+      <View style={styles.container}>
+        <Text>Open up App.tsx to start working on your app!</Text>
+        <Button mode="contained" >Click Me</Button>
+        <TextInput label="Email" /> 
+        <StatusBar style="auto" />
+      </View>
+    </Provider>
   );
 }
 
---
+++ src/core/theme.ts
@@ -0,0 +1,18 @@
+import { DefaultTheme } from "react-native-paper";
+
+export const theme = {
+    ...DefaultTheme,
+    colors: {
+        ...DefaultTheme.colors,
+        text: '#000000',
+        primary: '#121330',
+        tint: '#FCFCFD',
+        secondary: '#414757',
+        border: '#C9DDEC',
+        error: '#FF0000',
+        success: '#00B386',
+        google: '#2E7D32'
+    }
+
+
+}
